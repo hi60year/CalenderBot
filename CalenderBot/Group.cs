7a7ae47d73e90ffbbc76dc9e3ae934e5a88b7062
Constructor <18350282205@163.com>
2021-03-16 16:41:22 +0800

    update daysafter feature.

---
+++ CalenderBot/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nito.Collections;
+using Mirai_CSharp;
+using Mirai_CSharp.Models;
+
+namespace CalenderBot
+{
+    class Group
+    {
+        public static Dictionary<long, Group> Groups;
+        public Deque<IGroupMessageEventArgs> MessageQueue { get; set; } = new Deque<IGroupMessageEventArgs>();
+        public int MaxMessageNum { get; set; } = 100;
+        public long GroupNum;
+
+        static Group()
+        {
+            Groups = new Dictionary<long, Group>();
+        }
+        public Group(long gpNum)
+        {
+            GroupNum = gpNum;
+        }
+    }
+}
---
+++ CalenderBot/MessageQueueListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mirai_CSharp.Models;
+using Mirai_CSharp;
+
+namespace CalenderBot
+{
+    static class MessageQueueListener
+    {
+        public static async Task<bool> Listener(MiraiHttpSession session, IGroupMessageEventArgs e)
+        {
+            _ = Group.Groups.TryAdd(e.Sender.Group.Id, new Group(e.Sender.Group.Id));
+            var curgp = Group.Groups[e.Sender.Group.Id];
+            if (curgp.MessageQueue.Count <= curgp.MaxMessageNum)
+            {
+                curgp.MessageQueue.AddToBack(e);
+            }
+            else
+            {
+                curgp.MessageQueue.RemoveFromFront();
+                curgp.MessageQueue.AddToBack(e);
+            }
+            return false;
+        }
+    }
+}
