a5421bcf946dd4cbe5b2fc173e7ea4ede1c7ee47
constructor <dingjun-work@outlook.com>
2021-03-26 20:07:23 +0800

    fix the bug of throwing uncaught exception when a user use generate command before configuration.

--- CalenderBot/Commands/GenerateCalender.cs
+++ CalenderBot/Commands/GenerateCalender.cs
@@ -10,8 +10,15 @@ namespace CalenderBot.Commands
 {
     static class GenerateCalender
     {
+        static int CurrentGenerateRequestNum = 0;
         public static async Task Run(CommandParser.GenerateCalender args, MiraiHttpSession session, IGroupMessageEventArgs e)
         {
+            if (++CurrentGenerateRequestNum > 3)
+            {
+                await session.SendGroupMessageAsync(e.Sender.Group.Id, new PlainMessage("目前请求过多。请稍后再试"));
+                CurrentGenerateRequestNum--;
+                return;
+            }
             if (System.IO.File.Exists(@$"{Config.RootPath}\Users\{e.Sender.Id}\calender.json"))
             {
                 try
@@ -22,6 +29,7 @@ namespace CalenderBot.Commands
                 {
                     await session.SendGroupMessageAsync(e.Sender.Group.Id, new PlainMessage("原有的课程表文件存在，但删除失败，请稍后再试。" +
                         "如果这个问题反复存在，请联系管理员。"));
+                    CurrentGenerateRequestNum--;
                     return;
                 }
             }
@@ -31,16 +39,18 @@ namespace CalenderBot.Commands
             process.StartInfo.CreateNoWindow = false;
             process.StartInfo.RedirectStandardInput = true;
             var user = await User.TryLoad(e.Sender.Id);
+            if (user == null)
+            {
+                await session.SendGroupMessageAsync(e.Sender.Group.Id,
+                                    new PlainMessage("您未正确配置信息或无法正确加载用户信息，请使用cl config命令配置后再尝试生成，如果该问题持续存在，请联系管理员。"));
+                CurrentGenerateRequestNum--;
+                return;
+            }
             if (!user.Configed)
             {
                 await session.SendGroupMessageAsync(e.Sender.Group.Id,
                                                     new PlainMessage("您未正确配置信息，请使用cl config命令配置后再尝试生成。"));
-                return;
-            }
-            if (user == null)
-            {
-                await session.SendGroupMessageAsync(e.Sender.Group.Id,
-                                    new PlainMessage("无法正确加载用户信息，请稍后重试，如果该问题持续存在，请联系管理员。"));
+                CurrentGenerateRequestNum--;
                 return;
             }
             process.Start();
@@ -70,6 +80,7 @@ $@"echo ""{user.Username} {user.Password}
                                                         )}, msgid);
                 }
             );
+            CurrentGenerateRequestNum--;
         }
     }
 }
